f244824ffdf8484f9aac572524760708a674217b
Remaker00 <93196599+Remaker00@users.noreply.github.com>
2023-09-23 14:43:36 +0530

    version1.0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Expense from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div className="App">
+      <Expense />
+    </div>
+  );
+}
+
+export default App;
